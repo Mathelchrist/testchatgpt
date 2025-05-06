058e88c354da80a7007e42656b3b7a68729ff7e8
Mathelchrist <42696648+Mathelchrist@users.noreply.github.com>
2025-05-06 13:46:46 +0200

    2nd

--- components/ui/MealTrackerApp.jsx
+++ components/ui/MealTrackerApp.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../../components/ui/card";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
 
 const daysOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 

e3d7c9ddf3ebbb048504f28b030e866a26cc8f32
Mathelchrist <42696648+Mathelchrist@users.noreply.github.com>
2025-05-06 14:16:13 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,12 @@ import MealTrackerApp from '../components/ui/MealTrackerApp'
 
 export default function Home() {
   return (
-    <div>
+    <div style={{
+      backgroundImage: 'url(/background.png)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '100vh'
+    }}>
       <Head>
         <title>Plan Alimentaire</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
